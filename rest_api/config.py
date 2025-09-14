711aeee8e67969119e952190d7f1b9591ca0e1ad
lovehedelius <love@hedelius.net>
2025-09-14 17:43:37 +0200

    Update config.py

--- rest_api/config.py
+++ rest_api/config.py
@@ -1,5 +1,5 @@
 import os
 
-# Get the absolute path to lab3.sqlite, assuming it's in the parent directory of rest_api/
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Gets the rest_api directory
-DB_PATH = os.path.join(BASE_DIR, "..", "project.sqlite")  # Points to lab3.sqlite
+# Get the absolute path to project.sqlite, assuming it's in the parent directory of rest_api/
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "..", "project.sqlite")
